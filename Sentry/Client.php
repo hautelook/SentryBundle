f5ec119277729c6b032d0facf0e66793eef09ad0
Adrien Brault <adrien.brault@gmail.com>
2014-05-27 13:22:30 -0700

    Fix 0.1.6

--- Sentry/Client.php
+++ Sentry/Client.php
@@ -9,13 +9,15 @@ class Client extends BaseClient
 {
     public function __construct(array $config = array())
     {
-        if (!array_key_exists('php_version', $config)) {
-            $config['php_version'] = phpversion();
+        if (!isset($config['command']['params']['tags']['php_version'])) {
+            $config['command']['params']['tags']['php_version'] = phpversion();
         }
-        if (!array_key_exists('symfony_version', $config)) {
-            $config['symfony_version'] = Kernel::VERSION;
+        if (!isset($config['command']['params']['tags']['symfony_version'])) {
+            $config['command']['params']['tags']['symfony_version'] = Kernel::VERSION;
         }
-        $config['server_name'] = gethostname();
+        if (!isset($config['command']['params']['server_name'])) {
+            $config['command']['params']['server_name'] = gethostname();
+        }
 
         parent::__construct($config);
     }
